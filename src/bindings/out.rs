f722d52944835ac9d1752f034f986a82145c1478
lights0123 <developer@lights0123.com>
2019-04-20 14:54:30 -0400

    Fix type issue

--- src/bindings/out.rs
+++ src/bindings/out.rs
@@ -3,14 +3,14 @@ use core::fmt::{Error, Write};
 pub fn print(msg: &str) {
     unsafe {
         // Instead of allocating a C-style string, we tell printf the length to output
-        ndless_sys::printf("%.*s\0".as_ptr() as *const i8, msg.len(), msg);
+        ndless_sys::printf("%.*s\0".as_ptr() as *const cty::c_char, msg.len(), msg);
     }
 }
 
 pub fn println(msg: &str) {
     unsafe {
         // Instead of allocating a C-style string, we tell printf the length to output
-        ndless_sys::printf("%.*s\n\0".as_ptr() as *const i8, msg.len(), msg);
+        ndless_sys::printf("%.*s\n\0".as_ptr() as *const cty::c_char, msg.len(), msg);
     }
 }
 
