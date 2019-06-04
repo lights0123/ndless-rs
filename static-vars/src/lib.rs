aeb0906efbd5ae485748fa72aa43f97bea1475bf
lights0123 <developer@lights0123.com>
2019-06-04 15:00:30 -0400

    Allow comparing ProgramState

--- static-vars/src/lib.rs
+++ static-vars/src/lib.rs
@@ -1,8 +1,15 @@
 #![no_std]
 
+#[derive(Eq, PartialEq, Copy, Clone, Debug, Hash)]
 pub enum ProgramState {
 	Normal,
 	Resident,
+}
+
+impl Default for ProgramState {
+	fn default() -> Self {
+		ProgramState::Normal
+	}
 }
 
 pub static mut PROGRAM_STATE: ProgramState = ProgramState::Normal;
