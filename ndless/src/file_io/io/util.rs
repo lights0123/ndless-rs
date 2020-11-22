99002c564ace519e7f114a880249083fd051bc22
lights0123 <developer@lights0123.com>
2020-11-22 10:48:57 -0500

    Make compatible with new MaybeUninit

--- ndless/src/file_io/io/util.rs
+++ ndless/src/file_io/io/util.rs
@@ -45,24 +45,27 @@ where
 	W: Write,
 {
 	let mut buf = MaybeUninit::<[u8; super::DEFAULT_BUF_SIZE]>::uninit();
-	// FIXME(#53491): This is calling `get_mut` and `get_ref` on an uninitialized
-	// `MaybeUninit`. Revisit this once we decided whether that is valid or not.
-	// This is still technically undefined behavior due to creating a reference
-	// to uninitialized data, but within libstd we can rely on more guarantees
-	// than if this code were in an external lib.
+	// FIXME: #42788
+	//
+	//   - This creates a (mut) reference to a slice of
+	//     _uninitialized_ integers, which is **undefined behavior**
+	//
+	//   - Only the standard library gets to soundly "ignore" this,
+	//     based on its privileged knowledge of unstable rustc
+	//     internals;
 	unsafe {
-		reader.initializer().initialize(buf.get_mut());
+		reader.initializer().initialize(buf.assume_init_mut());
 	}
 
 	let mut written = 0;
 	loop {
-		let len = match reader.read(unsafe { buf.get_mut() }) {
+		let len = match reader.read(unsafe { buf.assume_init_mut() }) {
 			Ok(0) => return Ok(written),
 			Ok(len) => len,
 			Err(ref e) if e.kind() == ErrorKind::Interrupted => continue,
 			Err(e) => return Err(e),
 		};
-		writer.write_all(unsafe { &buf.get_ref()[..len] })?;
+		writer.write_all(unsafe { &buf.assume_init_ref()[..len] })?;
 		written += len as u64;
 	}
 }
